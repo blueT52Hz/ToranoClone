f56744c5e3c9d4eefc4b2cb978041e5b8e2d6fce
bluet52hzzz <lionel32204@gmail.com>
2025-01-26 23:42:12 +0700

    feat(WEB-11): thêm ci/cd deploy firebase

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,14 +2,8 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div
-      className="text-red-500 bg-slate-300 font-bold cursor-pointer"
-      onClick={() => {
-        console.log("aldfjasdhfkad");
-      }}
-      onChange={() => {}}
-    >
-      Login
+    <div className="text-red-500 bg-slate-300 font-bold cursor-pointer">
+      Login Page
     </div>
   );
 };
