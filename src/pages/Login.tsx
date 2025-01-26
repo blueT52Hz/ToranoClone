72ff0a83b2f84604fabd3be97ff5108b371e177a
bluet52hzzz <lionel32204@gmail.com>
2025-01-26 16:56:03 +0700

    feat(WEB-11): set up code base + ci/cd deploy vercel

---
+++ src/pages/Login.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div
+      className="text-red-500 bg-slate-300 font-bold cursor-pointer"
+      onClick={() => {
+        console.log("aldfjasdhfkad");
+      }}
+      onChange={() => {}}
+    >
+      Login
+    </div>
+  );
+};
+
+export default Login;
