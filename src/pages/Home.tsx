66d40d99a22dd4681dffddb8b16a327a51df0a04
bluet52hzzz <lionel32204@gmail.com>
2025-02-16 13:57:33 +0700

    feat(feature/WEB-20): Hoàn thiện Danh mục sản phẩm

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/HeroSection/HeroSection";
+import CategorySection from "@/components/Home_Category/CategorySection";
 import { Flex } from "antd";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <HeroSection></HeroSection>
+      <HeroSection />
+      <CategorySection />
     </>
   );
 };
